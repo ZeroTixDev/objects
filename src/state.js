f220b78d74c82be08a0c61e8a3b7334027568d5a
ZeroTix <weddnremy@gmail.com>
2020-12-14 20:43:17 -0500

    minor changes

--- src/state.js
+++ src/state.js
@@ -1,9 +1,7 @@
 'use strict';
-const Object = require('./object');
 module.exports = class State {
    constructor(state) {
       this.objects = state.objects;
-      this.undoCheck = false;
    }
    merge(newState) {
       const objects = [...this.objects, ...newState.objects];
